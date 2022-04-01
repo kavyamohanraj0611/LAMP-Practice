76f59452ba5fed4c8465712914c776daee009c96
kavyamohanraj0611 <96459811+kavyamohanraj0611@users.noreply.github.com>
2022-04-01 15:12:53 +0530

    Delete queryString.js

--- queryString.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var url = require('url');
-
-http.createServer(function (req, res) {
-  url
-  var que = url.parse(req.url, true).query;
-  var txt = que.year + " " + que.month;
-  res.end(txt);
-}).listen(8080);
