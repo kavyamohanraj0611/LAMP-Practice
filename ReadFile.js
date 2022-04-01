c788f5f779d725237dce71a02851473df00859a0
kavyamohanraj0611 <96459811+kavyamohanraj0611@users.noreply.github.com>
2022-04-01 14:57:23 +0530

    Delete ReadFile.js

--- ReadFile.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('OSI.html', function(err, data) {
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080); 
