2c3a68bc670e7e9be6cb85f65637a18ce8fa2ec5
kavyamohanraj0611 <96459811+kavyamohanraj0611@users.noreply.github.com>
2022-04-04 17:06:36 +0530

    Delete currDateDisp.js

--- NodeJs/currDateDisp.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var currDate = require('./currDateTime');
-
-http.createServer(function (req, res) {
-  res.write("The current date and time : " + currDate.dateTimefunc());
-  res.end();
-}).listen(8000);
