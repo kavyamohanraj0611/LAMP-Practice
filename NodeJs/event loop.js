1757e28f08d6ab3fb0b94fbcc02cc98f4c11c417
kavyamohanraj0611 <96459811+kavyamohanraj0611@users.noreply.github.com>
2022-04-05 14:34:01 +0530

    Add files via upload

---
+++ NodeJs/event loop.js
@@ -0,0 +1,23 @@
+console.log(1);
+
+setTimeout(()=> {
+    console.log('Welcome');
+}, 5000);
+
+setTimeout(()=> {
+    console.log('Hello');
+}, 2500);
+
+setTimeout(()=> {
+    console.log('Hi')
+}, 1000);
+
+for (const value of ['A','B','C']) {
+    console.log(value);
+}
+
+function two() {
+    console.log(2);
+}
+
+two();
