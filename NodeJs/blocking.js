00e025d342a6b0d8da98acb8e782977ae13471df
kavyamohanraj0611 <96459811+kavyamohanraj0611@users.noreply.github.com>
2022-04-27 11:36:46 +0530

    Delete blocking.js

--- NodeJs/blocking.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const data = fs.readFileSync('./demo.txt',{encoding: 'utf8'});
-console.log(data);
-
-let sum = 0;
-for(let i=1; i<=10; i++){
-	sum = sum + i;
-}
-console.log('Sum: ', sum);
