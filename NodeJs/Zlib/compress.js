d5fcc909b320b7e0f3c33661b34169dd068fe999
kavyamohanraj0611 <96459811+kavyamohanraj0611@users.noreply.github.com>
2022-04-27 10:16:22 +0530

    Update compress.js

--- NodeJs/Zlib/compress.js
+++ NodeJs/Zlib/compress.js
@@ -1,8 +1,9 @@
 //Compression
+
 const zlib = require("zlib");
 const fs = require('fs');
-const input = fs.createReadStream('demo.txt');
-const output = fs.createWriteStream('demo.txt.gz');
+const input = fs.createReadStream('./largeFile11.txt');
+const output = fs.createWriteStream('compressed_file.txt.gz');
 const gzip = zlib.createGzip();
 input.pipe(gzip).pipe(output);
 console.log("Done");
