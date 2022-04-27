1c8d571f1bde302469951c3c3eb8e23123f4d40c
kavyamohanraj0611 <96459811+kavyamohanraj0611@users.noreply.github.com>
2022-04-27 10:17:15 +0530

    Update decompress.js

--- NodeJs/Zlib/decompress.js
+++ NodeJs/Zlib/decompress.js
@@ -3,7 +3,7 @@
 const zlib = require("zlib");
 const fs = require('fs');
 const unzip=zlib.createUnzip();
-const input = fs.createReadStream('demo.txt.gz');
-const output = fs.createWriteStream('demo1.txt');
+const input = fs.createReadStream('./compressed_file.txt.gz');
+const output = fs.createWriteStream('largefile11.txt');
 input.pipe(unzip).pipe(output);
 console.log("Done");
