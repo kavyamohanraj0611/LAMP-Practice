e74df0afbe1eecf6378964ed77b85b0ad54c7eb2
kavyamohanraj0611 <96459811+kavyamohanraj0611@users.noreply.github.com>
2022-04-27 09:01:54 +0530

    Create queryString.js

---
+++ NodeJs/QueryString/queryString.js
@@ -0,0 +1,11 @@
+//URL query to Objects
+const queryString=require('querystring')
+let urlQuery="user=kavya&access=true&role=trainee&id=1234&practice=LAMP"
+let parsedObject = queryString.parse(urlQuery);
+console.log("Parsed Object:", parsedObject);
+  
+
+//Objects to URL query
+let urlObject = {user: "kavya",access: true,role: "trainee",id:'1234',practice:'LAMP'};
+let parsedQuery = queryString.stringify(urlObject);
+console.log("\nParsed Query:", parsedQuery);
